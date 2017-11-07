247ec55413828919a59c0bdc0c8e70280a539719
shrey <Shrey.Bhatnagar@infinite.com>
2017-11-07 13:49:26 +0530

    Added NAME - feature

--- c-project/main.c
+++ c-project/main.c
@@ -6,11 +6,16 @@
 
 int main(){
 int a=2, b=8, sum=0;
+char name[10];
 sum = add(a,b);
 printf("add: the Sum of num %d + %d is %d\n",a,b,sum);
 sum = sub(a,b);
 printf("sub: the Sum of num %d - %d is %d\n",a,b,sum);
 
-printf("\n\n ---- Finished the run!!!\n")
+printf("\n\nplease enter your name: ");
+scanf("%s",&name);
+printf("enterered name as %s",name);
+
+printf("\n\n ---- Finished the run!!!\n");
 return 0;
 }
